091b048395f0853c83673efebeae8a85adc8b68c
mnogoruk <lionless072@gmail.com>
2020-02-02 20:46:14 +0300

    Добавлены view функции, новые шаблоны(пустые), обновлён urls.py, почти всё готово

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,14 @@ urlpatterns = [
  path('main', main_view, name='main'),
  path('group/<int:group_id>', group_detail, name='group_detail'),
  path('group/create', group_create, name='group_create'),
- path('group/edit', group_edit, name='group_edit')
-
+ path('group/edit/<int:group_id>', group_edit, name='group_edit'),
+ path('teacher/<int:teacher_id>', teacher_schedule, name='teacher_schedule'),
+ path('teacher/create', teacher_create, name='teacher_create'),
+ path('teacher/edit/<int:teacher_d>/', teacher_edit, name='teacher_edit'),
+ path('student/create', student_create, name='student_create'),
+ path('student/edit/<int:student_id>', student_edit, name='student_edit'),
+ path('discipline/create', discipline_create, name='discipline_create'),
+ path('discipline/edit/<int:discipline_id>', discipline_edit, name='discipline_edit'),
+ path('programme/create', programme_create, name='programme_create'),
+ path('programme/edit/<int:programme_id>', programme_edit, name='programme_edit')
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ import time
 import json
 import os
 import datetime
+import uuid
 
 
 def find_person(login_name, password):
@@ -128,7 +129,7 @@ def add(to, list_name, element):
 
     data[list_name].append(element)
     with open(to, 'w', encoding='utf8') as file:
-        json.dump(data, file)
+        json.dump(data, file, ensure_ascii=False)
 
     return 1
 
@@ -169,7 +170,8 @@ def id_dict(frm, list_name):
 
 
 def main_view(request):
-    day = days_of_week[datetime.datetime.now().weekday()]
+    #day = days_of_week[datetime.datetime.now().weekday()]
+    day = 'вторник'
     with open('Schedule.json', 'r', encoding='utf8') as file:
         data = json.load(file)
 
@@ -193,52 +195,286 @@ def main_view(request):
 
 
 def group_detail(request, group_id):
-    group = get('Schedule.json', 'Schedule', group_id)
-    return redirect()
+    group = get('Groups.json', 'groups', group_id)
+    schedule = get('Schedule.json', 'Schedule', group_id, 'group')
+    disciplines = id_dict('ED_Disciplines.json', 'ed_disciplines')
+    teachers = id_dict('Teachers.json', 'teachers')
+    for discipline in disciplines:
+        disciplines[discipline]['teacher'] = teachers[disciplines[discipline]['teacher']]
+    for day in schedule['days']:
+        for tm in day['times']:
+            tm['discipline'] = disciplines[tm['discipline']]
+    schedule['group'] = group
+
+    return render(request, 'group_detail.html', {'schedule': schedule})
+
+
+def programme_finder():
+    programmes = id_dict('Ed_Programm.json', 'ed_programmes')
+    choices = []
+    for pr in programmes:
+        choices.append((pr, programmes[pr]['title']))
+    return choices
+
+
+class GroupForm(forms.Form):
+
+    number = forms.CharField(label='Номер')
+    ed_programme = forms.TypedChoiceField(choices=programme_finder, coerce=int)
+
 
 def group_create(request):
-    pass
+
+    if request.method == 'GET':
+        form = GroupForm()
+        return render(request, 'group_create.html', {'form': form})
+
+    if request.method == 'POST':
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            new_group = form.cleaned_data
+            new_group['id'] = uuid.uuid4().int
+            new_group['students'] = []
+            add('Groups.json', 'groups', new_group)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'group_create.html', {'form': form})
 
 
 def group_edit(request, group_id):
-    pass
+    group = get('Groups.json', 'groups', group_id)
+
+    if request.method == 'GET':
+        form = GroupForm(group)
+        return render(request, 'group_edit.html', {'form': form})
+
+    if request.method == 'POST':
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            group.update(form.cleaned_data)
+            edit('Groups.json', 'groups', group)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'group_edit.html', {'form': form})
 
 
 def teacher_schedule(request, teacher_id):
-    pass
+    teacher = get('Teachers.json', 'teachers', teacher_id)
+    schedules = id_dict('Schedule.json', 'Schedule')
+    groups = id_dict('Groups.json', 'groups')
+    disciplines = id_dict('Ed_Disciplines.json', 'ed_disciplines')
+
+    schd = {}
+    for schedule in schedules.values():
+        for day in schedule['days']:
+            for tm in day['times']:
+                if disciplines[tm['discipline']]['teacher'] == teacher_id:
+                    tm['discipline'] = disciplines[tm['discipline']]
+                    if day['day'] in schd:
+                        schd[day['day']].append(tm)
+                    else:
+                        schd[day['day']] = [tm]
+
+    return render(request, 'teacher.html', {'schedule': schd, 'teacher': teacher})
 
 
 def admin(request):
     pass
 
 
+statuses = (
+    ('Assistant', 'Assistant'),
+    ('Associate Professor', 'Associate Professor'),
+)
+
+
+class TeacherForm(forms.Form):
+
+    full_name = forms.CharField()
+    status = forms.ChoiceField(choices=statuses)
+
+
 def teacher_create(request):
-    pass
+    if request.method == 'GET':
+        form = TeacherForm()
+        return render(request, 'teacher_create.html', {'form': form})
+
+    if request.method == 'POST':
+        form = TeacherForm(request.POST)
+        if form.is_valid():
+            new_teacher = form.cleaned_data
+            new_teacher['id'] = uuid.uuid4().int
+            add('Teachers.json', 'teachers', new_teacher)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'teacher_create.html', {'form': form})
+
+
+def group_finder():
+    groups = id_dict('Groups.json', 'groups')
+    choices = []
+    for gr in groups:
+        choices.append((gr, groups[gr]['number']))
+    return choices
+
+
+class StudentForm(forms.Form):
+
+    full_name = forms.CharField()
+    group = forms.TypedChoiceField(choices=group_finder, coerce=int)
 
 
 def student_create(request):
-    pass
+    if request.method == 'GET':
+        form = StudentForm()
+        return render(request, 'student_create.html', {'form': form})
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            new_student = form.cleaned_data
+            _id = uuid.uuid4().int
+            new_student['id'] = _id
+            group = get('Groups.json', 'groups', new_student['group'])
+            group['students'].append(_id)
+            edit('Groups.json', 'groups', group)
+            add('Teachers.json', 'teachers', new_student)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'teacher_create.html', {'form': form})
 
 
-def teacher_edit(request):
-    pass
+def teacher_edit(request, teacher_id):
+    teacher = get('Teachers.json', 'teachers', teacher_id)
+
+    if request.method == 'GET':
+        form = TeacherForm(teacher)
+        return render(request, 'teacher_edit.html', {'form': form})
+
+    if request.method == 'POST':
+        form = TeacherForm(request.POST)
+        if form.is_valid():
+            teacher.update(form.cleaned_data)
+            edit('Teachers.json', 'teachers', teacher)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'group_edit.html', {'form': form})
 
 
-def student_edit(request):
-    pass
+def student_edit(request, student_id):
+    student = get('Student.json', 'students', student_id)
+
+    if request.method == 'GET':
+        form = StudentForm(student)
+        return render(request, 'student_edit.html', {'form': form})
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student.update(form.cleaned_data)
+            edit('Student.json', 'students', student)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'student_edit.html', {'form': form})
+
+
+work_types = (
+    ('Practical work', 'Practical work'),
+    ('Labolatory work', 'Labolatory work'),
+    ('Lecture', 'Lecture')
+)
+
+
+def teacher_finder():
+    teachers = id_dict('Teachers.json', 'teachers')
+
+    choices = []
+    for t, d in teachers.items():
+        choices.append((t, d['full_name']))
+
+    return choices
+
+
+class DisciplineForm(forms.Form):
+    title = forms.CharField()
+    type_of_work = forms.ChoiceField(choices=work_types)
+    teacher = forms.TypedChoiceField(choices=teacher_finder, coerce=int)
+    ed_programme = forms.TypedChoiceField(choices=programme_finder, coerce=int)
+    term = forms.ChoiceField(choices=((x, x) for x in range(1, 8)))
 
 
 def discipline_create(request):
-    pass
+    if request.method == 'GET':
+        form = DisciplineForm()
+        return render(request, 'discipline_create.html', {'form': form})
+
+    if request.method == 'POST':
+        form = DisciplineForm(request.POST)
+        if form.is_valid():
+            new_discipline = form.cleaned_data
+            _id = uuid.uuid4().int
+            new_discipline['id'] = _id
+            programme = get('Ed_Programm', 'ed_programmes', new_discipline['ed_programme'])
+            programme['ed_disciplines'].append(_id)
+            edit('Ed_Programm', 'ed_programmes', programme)
+            add('Ed_Disciplines.json', 'ed_disciplines', new_discipline)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'discipline_create.html', {'form': form})
 
 
-def discipline_edit(request):
-    pass
+def discipline_edit(request, discipline_id):
+    discipline = get('Ed_Disciplines.json', 'ed_disciplines', discipline_id)
+
+    if request.method == 'GET':
+        form = DisciplineForm(discipline)
+        return render(request, 'discipline_edit.html', {'form': form})
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            discipline.update(form.cleaned_data)
+            edit('Ed_Disciplines.json', 'ed_disciplines', discipline)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'discipline_edit.html', {'form': form})
+
+
+class ProgrammeForm(forms.Form):
+    title = forms.CharField()
+    description = forms.CharField(widget=forms.TextInput)
+    unique_code = forms.CharField()
 
 
 def programme_create(request):
-    pass
+    if request.method == 'GET':
+        form = ProgrammeForm()
+        return render(request, 'programme_create.html', {'form': form})
+
+    if request.method == 'POST':
+        form = ProgrammeForm(request.POST)
+        if form.is_valid():
+            new_programme = form.cleaned_data
+            new_programme['id'] = uuid.uuid4().int
+            new_programme['ed_discipline'] = []
+            add('ED_Programm.json', 'ed_programmes', new_programme)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'programme_create.html', {'form': form})
 
 
-def programme_edit(request):
-    pass
+def programme_edit(request, programme_id):
+    programme = get('Ed_Programm.json', 'students', programme_id)
+
+    if request.method == 'GET':
+        form = ProgrammeForm()
+        return render(request, 'programme_edit.html', {'form': form})
+
+    if request.method == 'POST':
+        form = ProgrammeForm(request.POST)
+        if form.is_valid():
+            programme.update(form.cleaned_data)
+            edit('Ed_Programm.json', 'ed_programmes', programme)
+            return redirect(reverse('main'))
+        else:
+            return render(request, 'programme_edit.html', {'form': form})
